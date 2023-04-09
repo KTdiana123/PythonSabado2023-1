105a2717542eb37574dd4603d6652956910f8652
Diana Vargas <dianakt2595@gmail.com>
2023-04-09 00:03:53 -0500

    nt2 se imprime con pf ne name y se crea carpeta de controladores

---
+++ main.py
@@ -0,0 +1,16 @@
+#DATOS PRIMITIVOS  QUEMADOS(datos basicos)
+nombreUsuario="Diana"
+edadUsuario=33
+estaturaUsuario=1.62
+yaDesayuno=True
+direcionUsuario=input("Direcion Usuario")
+
+#Salidas en Consola
+print("Hola tu nombre es", nombreUsuario, "Bienvenido")
+print("tu edad es", edadUsuario, "Es una buena edad")
+print(f"Tu direccion es{direcionUsuario} vives cerca" )
+
+numero1=int(input("ingrese nuemro"))
+numero2=int(input("ingrese otro numero"))
+sumatorio=numero1+numero2
+print(f"el sumatorio es {sumatorio}" )
